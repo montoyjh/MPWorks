4fcd0419f259348069ac5eb6c3aa214fa1a0d0af
ctoher <toherc@gmail.com>
2016-07-19 12:46:19 -0400

    Update EoS thermal firetasks

--- mpworks/firetasks/eos_thermal_tasks.py
+++ mpworks/firetasks/eos_thermal_tasks.py
@@ -1,4 +1,9 @@
 from monty.os.path import zpath
+import os
+import json
+from pymongo import MongoClient
+import numpy as np
+from decimal import Decimal
 
 __author__ = 'Cormac Toher'
 
@@ -7,6 +12,7 @@ from fireworks.utilities.fw_serializers import FWSerializable
 from fireworks.core.firework import FireTaskBase, FWAction
 from pymatgen.io.vasp.inputs import Incar, Poscar
 from pymatgen.analysis.eqn_of_state_thermal.eos_vasp_setup import ModifiedVolumeStructureSet
+from pymatgen.analysis.eqn_of_state_thermal.eos_wf_run import eos_thermal_properties
 from fireworks.core.firework import Firework, Workflow
 from mpworks.firetasks.vasp_io_tasks import VaspWriterTask, VaspToDBTask
 from mpworks.firetasks.custodian_task import get_custodian_task
@@ -103,3 +109,148 @@ class SetupModifiedVolumeStructTask(FireTaskBase, FWSerializable):
             connections[-999+i*10] = [-998+i*10]
             wf.append(Workflow(fws, connections))
         return FWAction(additions=wf)        
+
+
+class AddEoSThermalDataToDBTask(FireTaskBase, FWSerializable):
+    _fw_name = "Add EoS Thermal Data to DB"
+
+    def run_task(self, fw_spec):
+        db_dir = os.environ['DB_LOC']
+        db_path = os.path.join(db_dir, 'tasks_db.json')
+        i = fw_spec['original_task_id']
+#        i = fw_spec['task_id']
+
+        with open(db_path) as f:
+            db_creds = json.load(f)
+        connection = MongoClient(db_creds['host'], db_creds['port'])
+        tdb = connection[db_creds['database']]
+        tdb.authenticate(db_creds['admin_user'], db_creds['admin_password'])
+        tasks = tdb[db_creds['collection']]
+        eos_thermal = tdb['eos_thermal']
+        i = 'mp-20'
+        ndocs = tasks.find({"original_task_id": i, 
+                            "state":"successful"}).count()
+#        ndocs = tasks.find({"task_id": i, 
+#                            "state":"successful"}).count()
+        existing_doc = eos_thermal.find_one({"relaxation_task_id" : i})
+#        existing_doc = eos_thermal.find_one({"task_id" : i})
+        if existing_doc:
+            print "Updating: " + i
+        else:
+            print "New material: " + i
+        d = {"analysis": {}, "error": [], "warning": []}
+        d["ndocs"] = ndocs
+        o = tasks.find_one({"task_id" : i},
+                           {"pretty_formula" : 1, "spacegroup" : 1,
+                            "snl" : 1, "snl_final" : 1, "run_tags" : 1})
+        if not o:
+            raise ValueError("Cannot find original task id")
+        # Get energy vs. volume from strained structures
+        d["strain_tasks"] = {}
+        #ss_dict = {}
+        volume_values = []
+        energy_values = []
+        for k in tasks.find({"original_task_id": i}, 
+                            {"strainfactor":1,
+                             "calculations.output":1,
+                             "state":1, "task_id":1}):
+            defo = k['strainfactor']
+            energy_values.append(item['output.final_energy'])
+            volume_values.append(item['output.crystal.lattice.volume'])
+            # d_ind = np.nonzero(defo - np.eye(3))
+            # delta = Decimal((defo - np.eye(3))[d_ind][0])
+            # Normal deformation
+            # if d_ind[0] == d_ind[1]:
+            #    dtype = "_".join(["d", str(d_ind[0][0]), 
+            #                      "{:.0e}".format(delta)])
+            # Shear deformation
+            # else:
+            #    dtype = "_".join(["s", str(d_ind[0] + d_ind[1]),
+            #                      "{:.0e}".format(delta)])
+            #sm = IndependentStrain(defo)
+            #d["strain_tasks"][dtype] = {"state" : k["state"],
+            #                                 "strainfactor" : defo,
+            #                                 "strain" : sm.tolist(),
+            #                                 "task_id": k["task_id"]}
+            #if k["state"] == "successful":
+            #    st = Stress(k["calculations"][-1]["output"] \
+            #                ["ionic_steps"][-1]["stress"])
+            #    ss_dict[sm] = st
+        d["snl"] = o["snl"]
+        if "run_tags" in o.keys():
+            d["run_tags"] = o["run_tags"]
+            for tag in o["run_tags"]:
+                if isinstance(tag, dict):
+                    if "input_id" in tag.keys():
+                        d["input_mp_id"] = tag["input_id"]
+        d["snl_final"] = o["snl_final"]
+        d["pretty_formula"] = o["pretty_formula"]
+
+        # Old input mp-id style
+        if o["snl"]["about"].get("_mp_id"):
+            d["material_id"] = o["snl"]["about"]["_mp_id"]
+
+        # New style
+        elif "input_mp_id" in d:
+            d["material_id"] = d["input_mp_id"]
+        else:
+            d["material_id"] = None
+        d["relaxation_task_id"] = i
+
+        calc_struct = Structure.from_dict(o["snl_final"])
+        # TODO:
+        # JHM: This test is unnecessary at the moment, but should be redone
+        """
+        conventional = is_conventional(calc_struct)
+        if conventional:
+            d["analysis"]["is_conventional"] = True
+        else:
+            d["analysis"]["is_conventional"] = False
+        """
+        d["spacegroup"]=o.get("spacegroup", "Unknown")
+
+        
+        if ndocs >= 21:
+            eos_thermal_dict = {}
+            # Perform thermal equation of state fitting and analysis
+            eos_thermal_dict = eos_thermal_properties.eos_thermal_run(calc_struct, volume_values, energy_values, ieos=2)
+            
+            # Add equation of state results to dict
+            d["temperature"] = eos_thermal_dict["temperature"]
+            d["pressure"] = eos_thermal_dict["pressure"]
+            d["Thermal_conductivity_temp_list"] = eos_thermal_dict["thermal_conductivity"]
+            d["Debye_temperature_temp_list"] = eos_thermal_dict["Debye_temperature"]
+            d["Gruneisen_parameter_temp_list"] = eos_thermal_dict["Gruneisen_parameter"]
+            d["Heat_capacity_Cv_temp_list"] = eos_thermal_dict["Heat_capacity_Cv"]
+            d["Heat_capacity_Cp_temp_list"] = eos_thermal_dict["Heat_capacity_Cp"]
+            d["Volume_temp_list"] = eos_thermal_dict["Volume"]
+            d["Bulk_modulus_temp_list"] = eos_thermal_dict["Bulk_modulus"]
+            d["BM_coeffs"] = eos_theraml_dict["Bulk_modulus"]
+            jtdbest = eos_thermal_dict["Best_fit_temperature"]
+
+            # Add values at specific temperatures (Debye temperature and Gruneisen parameter best fit, values at 300K)
+            d["Debye_temperature"] =  d["Debye_temperature_temp_list"][eos_thermal_dict["Best_fit_temperature"]]
+            d["Gruneisen_parameter"] = d["Gruneisen_parameter_temp_list"][eos_thermal_dict["Best_fit_temperature"]]
+            d["Thermal_conductivity_300K"] = d["Thermal_conductivity_temp_list"][eos_thermal_dict["300K_point"]]
+            d["Heat_capacity_Cv_300K"] = d["Heat_capacity_Cv_temp_list"][eos_thermal_dict["300K_point"]]
+            d["Heat_capacity_Cp_300K"] = d["Heat_capacity_Cp_temp_list"][eos_thermal_dict["300K_point"]]
+            d["Bulk_modulus_300K"] = d["Bulk_modulus_temp_list"][eos_thermal_dict["300K_point"]]
+
+
+
+        else:
+            d['state'] = "Fewer than 21 successful tasks completed"
+            return FWAction()
+
+        if o["snl"]["about"].get("_kpoint_density"):
+            d["kpoint_density"]= o["snl"]["about"].get("_kpoint_density")
+
+        if d["error"]:
+            raise ValueError("Thermal equation of state analysis failed: {}".format(d["error"]))
+        elif d["analysis"]["filter_pass"]:
+            d["state"] = "successful"
+        else:
+            d["state"] = "filter_failed"
+        eos_thermal.update({"relaxation_task_id": d["relaxation_task_id"]}, 
+                           d, upsert=True)
+        return FWAction()
